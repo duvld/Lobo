7b78aa65376967db05f11ab1a46f7d15e7259d0a
duvld <ollejna@gmail.com>
2017-03-01 15:31:36 -0500

    base backendless stuff

--- app/src/main/java/com/example/angietong/lobo/MainActivity.java
+++ app/src/main/java/com/example/angietong/lobo/MainActivity.java
@@ -2,12 +2,19 @@ package com.example.angietong.lobo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.backendless.Backendless;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String YOUR_APP_ID = "5B5E6CAE-0C25-19FE-FF24-600425E97500";
+    private static final String YOUR_SECRET_KEY = "DE82CF74-A7CF-9241-FF82-04CD84301800";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String appVersion = "v1";
+        Backendless.initApp( this, YOUR_APP_ID, YOUR_SECRET_KEY, appVersion );
     }
 }
