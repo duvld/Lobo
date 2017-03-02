835b5a44cf53b0440d8cab81f0c6cb56a666eb6f
duvld <ollejna@gmail.com>
2017-03-02 12:03:06 -0500

    test commit for slack

--- app/src/main/java/com/example/angietong/lobo/UI/MainActivity.java
+++ app/src/main/java/com/example/angietong/lobo/UI/MainActivity.java
@@ -25,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
     //
     // GoogleApiClient mGoogleApiClient = null;
 
-
+    //TEST COMMIT TEST COMMIT TEST COMMIT TEST COMMIT TEST COMMIT
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
