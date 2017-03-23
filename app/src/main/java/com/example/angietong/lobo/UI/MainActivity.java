21fa1c93a37dc3357319bbf8246ea2724d555d7e
duvld <ollejna@gmail.com>
2017-03-22 22:03:00 -0400

    COMMENTSWORKS

--- app/src/main/java/com/example/angietong/lobo/UI/MainActivity.java
+++ app/src/main/java/com/example/angietong/lobo/UI/MainActivity.java
@@ -22,10 +22,12 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -115,6 +117,10 @@ public class MainActivity extends AppCompatActivity {
     private String mBackendlessFileURL;
     // GoogleApiClient mGoogleApiClient = null;
 
+    //COMMENTS
+    private ListView mComments;
+
+
     // Create Map Style
     String styleString = "[{\"featureType\":\"water\",\"elementType\":\"geometry\",\"stylers\":[{\"hue\":\"#165c64\"},{\"saturation\":34},{\"lightness\":-69},{\"visibility\":\"on\"}]},{\"featureType\":\"landscape\",\"elementType\":\"geometry\",\"stylers\":[{\"hue\":\"#b7caaa\"},{\"saturation\":-14},{\"lightness\":-18},{\"visibility\":\"on\"}]},{\"featureType\":\"landscape.man_made\",\"elementType\":\"all\",\"stylers\":[{\"hue\":\"#cbdac1\"},{\"saturation\":-6},{\"lightness\":-9},{\"visibility\":\"on\"}]},{\"featureType\":\"road\",\"elementType\":\"geometry\",\"stylers\":[{\"hue\":\"#8d9b83\"},{\"saturation\":-89},{\"lightness\":-12},{\"visibility\":\"on\"}]},{\"featureType\":\"road.highway\",\"elementType\":\"geometry\",\"stylers\":[{\"hue\":\"#d4dad0\"},{\"saturation\":-88},{\"lightness\":54},{\"visibility\":\"simplified\"}]},{\"featureType\":\"road.arterial\",\"elementType\":\"geometry\",\"stylers\":[{\"hue\":\"#bdc5b6\"},{\"saturation\":-89},{\"lightness\":-3},{\"visibility\":\"simplified\"}]},{\"featureType\":\"road.local\",\"elementType\":\"geometry\",\"stylers\":[{\"hue\":\"#bdc5b6\"},{\"saturation\":-89},{\"lightness\":-26},{\"visibility\":\"on\"}]},{\"featureType\":\"poi\",\"elementType\":\"geometry\",\"stylers\":[{\"hue\":\"#c17118\"},{\"saturation\":61},{\"lightness\":-45},{\"visibility\":\"on\"}]},{\"featureType\":\"poi.park\",\"elementType\":\"all\",\"stylers\":[{\"hue\":\"#8ba975\"},{\"saturation\":-46},{\"lightness\":-28},{\"visibility\":\"on\"}]},{\"featureType\":\"transit\",\"elementType\":\"geometry\",\"stylers\":[{\"hue\":\"#a43218\"},{\"saturation\":74},{\"lightness\":-51},{\"visibility\":\"simplified\"}]},{\"featureType\":\"administrative.province\",\"elementType\":\"all\",\"stylers\":[{\"hue\":\"#ffffff\"},{\"saturation\":0},{\"lightness\":100},{\"visibility\":\"simplified\"}]},{\"featureType\":\"administrative.neighborhood\",\"elementType\":\"all\",\"stylers\":[{\"hue\":\"#ffffff\"},{\"saturation\":0},{\"lightness\":100},{\"visibility\":\"off\"}]},{\"featureType\":\"administrative.locality\",\"elementType\":\"labels\",\"stylers\":[{\"hue\":\"#ffffff\"},{\"saturation\":0},{\"lightness\":100},{\"visibility\":\"off\"}]},{\"featureType\":\"administrative.land_parcel\",\"elementType\":\"all\",\"stylers\":[{\"hue\":\"#ffffff\"},{\"saturation\":0},{\"lightness\":100},{\"visibility\":\"off\"}]},{\"featureType\":\"administrative\",\"elementType\":\"all\",\"stylers\":[{\"hue\":\"#3a3935\"},{\"saturation\":5},{\"lightness\":-57},{\"visibility\":\"off\"}]},{\"featureType\":\"poi.medical\",\"elementType\":\"geometry\",\"stylers\":[{\"hue\":\"#cba923\"},{\"saturation\":50},{\"lightness\":-46},{\"visibility\":\"on\"}]}]";
     public MapStyleOptions style = new MapStyleOptions(styleString);
@@ -160,6 +166,7 @@ public class MainActivity extends AppCompatActivity {
         mOkButton = (Button) findViewById(R.id.okButton);
         mNoButton = (Button) findViewById(R.id.noButton);
 
+
         main = (SlidingUpPanelLayout) findViewById(R.id.activity_main);
         main.setDragView(mMiniPost);
 
@@ -169,6 +176,7 @@ public class MainActivity extends AppCompatActivity {
         mBtmToolbar.setVisibility(View.VISIBLE);
 
         buildGoogleAPIClient();
+        commentsListView();
 
         //TEST setArray and getPostArray HERE
         getPostArray();
@@ -355,6 +363,20 @@ public class MainActivity extends AppCompatActivity {
             mMap.setMyLocationEnabled(true);
         }
 
+    }
+
+    private void commentsListView() {
+        String[] fakeComments = {"wow", "much fake", "ian eats sausage", "lul", "kiam eats tofu", "moegoe360 eats hummus",
+                "anji eats rice", "anji eats rice", "anji eats rice", "anji eats rice"};
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_1,
+                fakeComments
+        );
+
+        ListView list = (ListView) findViewById(R.id.comments);
+        list.setAdapter(adapter);
     }
 
     @Override
